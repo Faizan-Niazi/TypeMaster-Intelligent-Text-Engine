68561702714ae4668b814a3d377dc711dfda3c2a
Faizan-Niazi <faizanniazi001@gmail.com>
2025-12-14 08:11:58 +0500

    Update .gitignore to ignore IDE and build files

--- cpp_backend/src/main.cpp
+++ cpp_backend/src/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include "../include/trie.h"
-  
+  #include "../include/hash.h"
 using namespace std;
+void loadDictionary(string &path){
 
+    ifstream file(path);
+    if(!file.is_open()) {
+        cout<<"Failed to load Dictionary!......Exiting....";
+        return;
+    }
+    string line; int count=0;
+
+    while (getline(file,line))
+    {
+        if(line.empty()) continue;
+        
+        int pos1= line.find(',');
+        int pos2= line.find(',',pos1+1);
+
+        string word { line.substr(0,pos1)};
+        string type { line.substr(pos1+1,pos2-pos1-1)};
+        string definition {line.substr(pos2+1)};
+
+        Entry e{word,type,definition};
+
+        HashTable dictTable;
+        dictTable.insert(e); // for word type and definition lookup
+
+        Trie trie;
+        trie.insertWord(word);// for word lookpu
+
+        count++;
+        if(count%5000==0) cout<<count<<" words loaded";
+    }
+    
+}
 int main() {
+    string path="../dictionary.csv";
+    loadDictionary(path);
     Trie trie;
     trie.insertWord("apple");
     trie.insertWord("ape");
