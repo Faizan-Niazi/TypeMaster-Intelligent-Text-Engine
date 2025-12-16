d7628236abb14963ca368a0ab8d6e10c26f52c7d
abdulrafaynq <rwnido03@gmail.com>
2025-12-16 12:54:12 +0500

    Basic insert logic added

--- cpp_backend/src/heap.cpp
+++ cpp_backend/src/heap.cpp
@@ -6,3 +6,10 @@ MaxHeap::MaxHeap() {
 bool MaxHeap::isEmpty() {
     return arr.empty();
 }
+
+void MaxHeap::insert(const string& word, int score) {
+    HeapItem item;
+    item.word = word;
+    item.score = score;
+    arr.push_back(item);
+}
